a2a98fff59d65d71c284e5ae5df3d4ebab677d39
dev <devbhalani3184@gmail.com>
2024-09-07 14:54:58 +0530

    saple chage

--- frontend/src/Component/Contact.jsx
+++ frontend/src/Component/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <div>
         <section className='py-48'>
-            <h1 className=' text-3xl text-center'>contact</h1>
+            <h1 className=' text-3xl text-center'>Contact</h1>
         </section>
         
       
